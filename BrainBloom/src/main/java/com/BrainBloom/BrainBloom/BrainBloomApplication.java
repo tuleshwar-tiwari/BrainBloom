72d4de5e0bbfac714bed28c6ba1cea6407a8ed3d
unknown <ttuleshwar7@gmail.com>
2025-02-10 12:57:18 +0530

    Initial commit

---
+++ BrainBloom/src/main/java/com/BrainBloom/BrainBloom/BrainBloomApplication.java
@@ -0,0 +1,13 @@
+package com.BrainBloom.BrainBloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainBloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainBloomApplication.class, args);
+	}
+
+}
---
+++ BrainBloom/src/test/java/com/BrainBloom/BrainBloom/BrainBloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BrainBloom.BrainBloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainBloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
